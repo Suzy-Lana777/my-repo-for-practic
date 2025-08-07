fab858273aebf91eb1a9332d00c721dd17c57007
Suzy-Lana777 <133979206+Suzy-Lana777@users.noreply.github.com>
2025-08-07 15:49:28 +0300

    Initial commit

---
+++ lib/api.ts
@@ -0,0 +1,40 @@
+// lib/api.ts
+
+import axios from "axios";
+
+export type Note = {
+  id: string;
+  title: string;
+  content: string;
+  categoryId: string;
+  userId: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type NoteListResponse = {
+  notes: Note[];
+  total: number;
+};
+
+axios.defaults.baseURL = "https://next-docs-api.onrender.com";
+
+export const getNotes = async () => {
+  const res = await axios.get<NoteListResponse>("/notes");
+  return res.data;
+};
+
+// Інший код файлу
+
+
+export const getSingleNote = async (id: string) => {
+  const res = await axios.get<Note>(`/notes/${id}`);
+  return res.data;
+};
+// const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// export const getNotes = async () => {
+//   await delay(2000);
+//   const res = await axios.get<NoteListResponse>('/notes');
+//   return res.data;
+// };
